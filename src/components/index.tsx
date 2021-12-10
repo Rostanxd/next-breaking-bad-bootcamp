ec1be7941caac8e53de86bb740885f0d866a6800
Rostan <allowed_g@hotmail.com>
2021-12-09 22:22:46 -0500

    index.tsx file to export components was created

---
+++ src/components/index.tsx
@@ -0,0 +1,3 @@
+export { default as CharacterList } from './CharactersList';
+export { default as CharacterItem } from './CharacterItem';
+export { default as LoadingSpinner } from './LoadingSpinner';
