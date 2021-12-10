e293a511f02113e33e9942fb47ff4a727788c983
Rostan <allowed_g@hotmail.com>
2021-12-09 22:22:23 -0500

    LoadingSpinner component was created

---
+++ src/components/LoadingSpinner/index.tsx
@@ -0,0 +1,22 @@
+import styles from "./styles.module.css";
+
+const LoadingSpinner = () => {
+  return (
+    <div className={styles.ldsSpinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+}
+
+export default LoadingSpinner;
